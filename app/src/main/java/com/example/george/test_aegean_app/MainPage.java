976b1724515abc5c029fd20942ecd6a2b36306a8
georgemakrakis <georgemakrakis88@gmail.com>
2016-09-22 22:54:25 +0300

    Eighth Commit

--- app/src/main/java/com/example/george/test_aegean_app/MainPage.java
+++ app/src/main/java/com/example/george/test_aegean_app/MainPage.java
@@ -1,66 +1,119 @@
 package com.example.george.test_aegean_app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainPage extends ActionBarActivity {
+public class MainPage extends ActionBarActivity
+{
 
     private View v;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main_page, menu);
         return true;
     }
+
+    public void imageOnClick(View v)
+    {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.aegean.gr"));
+        startActivity(browserIntent);
+    }
+
     public void icsdOnClick(View v)
     {
         Intent i = new Intent(this, icsd_page.class);
         startActivity(i);
     }
+
     public void sitisiOnClick(View v)
     {
         Intent s = new Intent(this, Sitisi.class);
         startActivity(s);
     }
+
     public void saxmOnClick(View v)
     {
         Intent i = new Intent(this, saxm_page.class);
         startActivity(i);
     }
+
     public void mathOnClick(View v)
     {
         Intent i = new Intent(this, math_page.class);
         startActivity(i);
     }
+
     public void mapsClick(View v)
     {
-        Intent i= new Intent(this,MapsActivity.class);
-        startActivity(i);
+        //
+        int locationMode = Settings.Secure.getInt(
+                getContentResolver(),
+                Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF // Default value if not found
+        );
+
+        if (locationMode == Settings.Secure.LOCATION_MODE_OFF)
+        {
+            Dialog("Η χρήση της τοποθεσίας σας δεν είναι ενεργή");
+        }
+        else
+        {
+            Intent i = new Intent(this, MapsActivity.class);
+            startActivity(i);
+        }
+
     }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Dialog(String msg)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    //exits the application
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        return;
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
--- app/src/main/java/com/example/george/test_aegean_app/MapsActivity.java
+++ app/src/main/java/com/example/george/test_aegean_app/MapsActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -65,8 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -81,6 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -89,7 +90,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            Dialog("Η χρήση της τοποθεσίας σας δεν είναι ενεργή");
+
         }
         mMap.setMyLocationEnabled(true);//user's location
 
@@ -203,7 +204,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     //exits the application
                     public void onClick(DialogInterface dialog, int id)
                     {
-                       return;
+                        return;
                     }
                 });
         AlertDialog alert = builder.create();
